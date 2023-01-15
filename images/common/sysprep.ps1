ab0b9c9b554320265cbe34e8082aa62dc6a768d0
KAMEGAWA Kazushi <kkamegawa@clueup.org>
2023-01-15 16:39:14 +0900

    remove service check

--- images/common/sysprep.ps1
+++ images/common/sysprep.ps1
@@ -1,7 +1,5 @@
 Write-Output '>>> Waiting for GA Service (RdAgent) to start ...'
 while ((Get-Service RdAgent).Status -ne 'Running') { Start-Sleep -s 5 }
-Write-Output '>>> Waiting for GA Service (WindowsAzureTelemetryService) to start ...'
-while ((Get-Service WindowsAzureTelemetryService) -and ((Get-Service WindowsAzureTelemetryService).Status -ne 'Running')) { Start-Sleep -s 5 }
 Write-Output '>>> Waiting for GA Service (WindowsAzureGuestAgent) to start ...'
 while ((Get-Service WindowsAzureGuestAgent).Status -ne 'Running') { Start-Sleep -s 5 }
 Write-Output '>>> Sysprepping VM ...'
